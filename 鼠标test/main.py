150e7b0395f0956d9b582717ca63eafae8a2862d
吴仁杰 <wurenjie@datagrand.com>
2021-01-26 11:02:28 +0800

    修改模拟鼠标mian

--- 鼠标test/main.py
+++ 鼠标test/main.py
@@ -1,13 +1,33 @@
 # main.py -- put your code here!
 import pyb
+import math
+import random
 
 switch = pyb.Switch()
 accel = pyb.Accel()
 hid = pyb.USB_HID()
 
-f = 1
+def change_mouse():
+	while True:
+		# if switch.value()
+		hid.send((switch.value(), accel.x(), accel.y(), 0))
+		pyb.delay(20)
 
-while True:
-	# if switch.value()
-	hid.send((switch.value(), accel.x(), accel.y(), 0))
-	pyb.delay(20)
+def change_mouse_to(x, y, k, d):
+	for i in range(abs(x)):
+		hid.send((k, int(x/abs(x)), 0, 0))
+		pyb.delay(d)
+	for i in range(abs(y)):
+		hid.send((k, 0, int(y/abs(y)), 0))
+		pyb.delay(d)
+
+def change_mouse_to_x(x, y, k, d):
+	for i in range(int(abs(x)/5)):
+		hid.send((k, int(x/abs(x)*5), random.randint(-2,2), 0))
+		pyb.delay(d)
+	for i in range(int(abs(y)/5)):
+		hid.send((k, 0, int(y/abs(y)*5), 0))
+		pyb.delay(d)
+	change_mouse_to(50, 0, k, d)
+	change_mouse_to(-50, 0, k, d)
+	hid.send((0, 0, 0, 0))
