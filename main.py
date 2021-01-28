8955c5f97720c503db4de4512c457fe4ac4e1159
吴仁杰 <wurenjie@datagrand.com>
2021-01-28 11:27:55 +0800

    键盘测试

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 # main.py -- put your code here!
 import pyb
-from pyb import Switch
-def f():
-	pyb.delay(50)
-	if(sw()):
-		for i in range(1,5):
-			pyb.LED(i).toggle()
-			pyb.delay(200)
-sw=Switch()
-sw.callback(f)
+hid=pyb.USB_HID()
+def release_key_once():
+    buf = bytearray(8) # report is 8 bytes long
+    buf[2] = 0
+    hid.send(buf) # key released
+    pyb.delay(10)
+def press_key_once(key):
+    buf = bytearray(8) # report is 8 bytes long
+    buf[2] = key
+    hid.send(buf) # key released
+    pyb.delay(10)
+def press_2key(key1,key2):
+    buf = bytearray(8) # report is 8 bytes long
+    buf[0] = key1
+    buf[2] = key2
+    hid.send(buf) # key released
+    pyb.delay(10)
+def release_2key():
+    buf = bytearray(8) # report is 8 bytes long
+    buf[0] = 0
+    buf[2] = 0
+    hid.send(buf) # key released
+    pyb.delay(10)
